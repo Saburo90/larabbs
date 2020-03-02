e5e268cfc9530d8293c0802013f3e7a6032774db
root <saburo90@163.com>
2020-03-02 22:59:40 +0800

    基础页面构建

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="http://github.com/saburo/larabbs" target="_blank">LaraBBS</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:larabbs@example.com">联系我们</a></p>
+  </div>
+</footer>

2f3941595f0986867e7c91c5675bd9cf11032c20
root <saburo90@163.com>
2020-03-03 16:47:48 +0800

    上传头像类型验证bug修复

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,14 +28,14 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
-            'avatar' => 'mines:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
 
     public function messages()
     {
         return [
-            'avatar.mines' => '头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.mimes' => '头像必须是 jpeg, bmp, png, gif 格式的图片',
             'avatar.dimensions' => '头像清晰度不够，宽和高需要 208px 以上',
             'name.unique' => '用户名已被占有，请重新填写',
             'name.regex' => '用户名只支持英文数字横杠下划线',

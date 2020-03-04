f19737ad3a4dbd31f1949b1c88879b4011e2b47c
root <saburo90@163.com>
2020-03-04 17:48:27 +0800

    supervisor运行队列

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -26,10 +27,15 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+    }
 
+    public function saved(Topic $topic)
+    {
         // 如果话题slug字段无内容，则使用翻译器对 title进行翻译
-        if (! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        if (!$topic->slug) {
+//            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // 推送任务到翻译slug队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }

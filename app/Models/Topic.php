29e832f90ea64a58f5fd7dc164b6a5bab54be433
root <saburo90@163.com>
2020-03-04 11:12:01 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,22 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * 帖子与用户表关联关系
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * 帖子与帖子分类表的关联关系
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }

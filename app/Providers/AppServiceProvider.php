567a062201ff750d760e5e1917c91867a677b449
root <saburo90@163.com>
2020-03-04 10:06:40 +0800

    生成话题骨架

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+
         // Carbon 本地化 （中文：7天前)
         \Carbon\Carbon::setLocale('zh');
     }
